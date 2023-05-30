730b3e7a4fb05d536c4a3a55e4d6737643e881e4
diako83 <ismaildiako@gmail.com>
2023-05-30 10:21:56 +0200

    second upadate

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,17 +1,33 @@
 package org.example;
 
 
+import java.util.Objects;
+
 public class Calculator {
+
 
 
     public void  mainCalculator(){
         Services services = new Services();
+        double firstNumber = 0.0;
         boolean exit = true;
         while (exit) {
 
+            if (firstNumber > 0.0) {
+                System.out.println("Clear all values? press y. Else input any value end enter ");
+               if(Objects.equals(services.inputString(), "y")){
+                   firstNumber = 0.0;
+               }
+            }
+
 
             System.out.println("Welcome to quick calculator");
-            double firstNumber = services.inputDouble("Enter first number ");
+            if (firstNumber > 0.0) {
+                 double additional =       services.inputDouble("Enter first number ");
+               firstNumber = firstNumber + additional;
+            }
+            else
+            firstNumber = services.inputDouble("Enter first number ");
 
             char operator = services.inputOperator("Enter   +,  -  ,   /,   or  * ");
             System.out.println(operator);
